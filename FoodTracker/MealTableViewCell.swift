d8dbdb74205ae7799c8755efad73a4190f10cb9f
Jonathan <jonathanruedasmora@gmail.com>
2017-06-30 12:23:19 +0200

    Añadida lista de imagenes

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jonathan Ruedas Mora on 30/6/17.
+//  Copyright © 2017 Jonathan Ruedas Mora. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
